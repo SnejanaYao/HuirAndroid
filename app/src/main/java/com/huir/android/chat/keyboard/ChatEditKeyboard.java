02929fcd66cd208f3b7fff626223710d75ef0df4
karolina <karolina@karolina_PC>
2018-06-07 16:04:48 +0800

    迁移项目

---
+++ app/src/main/java/com/huir/android/chat/keyboard/ChatEditKeyboard.java
@@ -0,0 +1,44 @@
+package com.huir.android.chat.keyboard;
+
+import com.huir.android.chat.ChatViewAdapter;
+import com.huir.android.entity.Msg;
+
+import android.annotation.SuppressLint;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
+import android.widget.ListView;
+
+@SuppressLint("NewApi")
+public class ChatEditKeyboard implements OnKeyListener {
+	private ChatViewAdapter chatViewAdapter;
+	private ListView clist;
+	private EditText et_meg;
+	
+	
+
+	public ChatEditKeyboard(ChatViewAdapter chatViewAdapter, ListView clist, EditText et_meg) {
+		super();
+		this.chatViewAdapter = chatViewAdapter;
+		this.clist = clist;
+		this.et_meg = et_meg;
+	}
+
+
+
+	@Override
+	public boolean onKey(View v, int keyCode, KeyEvent event) {
+		String msg = et_meg.getText().toString();
+		if(keyCode == KeyEvent.KEYCODE_ENTER) {
+			if(!msg.isEmpty()) {
+				chatViewAdapter.addDataToAdapter(new Msg(msg, 1));
+				chatViewAdapter.notifyDataSetChanged();
+			    clist.smoothScrollToPosition(clist.getCount() - 1);
+			    et_meg.setText("");
+			    }
+			}
+		return false;
+	}
+
+}
